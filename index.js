b8f5163d767e31e4e0d774b7ac7930e0e5d51eb6
unknown <irisjitomo@yahoo.com>
2021-02-12 17:26:22 -0500

    cleaned up index.js, removed state and routes to access state within index.js

--- index.js
+++ index.js
@@ -5,47 +5,13 @@ require('dotenv').config();
 
 const server = express();
 
-// create some state
-
-const desserts = [
-	{
-		name: 'cake'
-	},
-	{
-		name: 'chocolate'
-	}
-];
-
 // middlewares (custom too)
-
-function logger(req, next){
-    // console.log(`Used ${req.method} request`)
-    console.log(`[${new Date().toISOString()}] ${req.method}`)
-    next();
-}
+server.use(helmet)
 server.use(express.json());
-// server.use(logger)
 
 // make get request to root endpoint '/'
 server.get('/', (res) => {
 	res.status(200).json({ message: 'Hello' });
-});
-
-// make get request to /whatever to access whatever state
-
-server.get('/dessert', (req, res) => {
-	res.status(200).json(desserts);
-});
-
-// make POST request to post to whatever state
-
-server.post('/dessert', (req, res) => {
-	if (req.body.name) {
-		desserts.push(req.body);
-		res.status(201).json(desserts);
-	} else {
-		res.status(404).json({ message: 'must contain a body' });
-	}
 });
 
 // listen to server on port 4000
